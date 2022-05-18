9f2773ce1c38f2ed3a363216c741490d60aa01dd
Cata2507 <105198441+Cata2507@users.noreply.github.com>
2022-05-18 13:46:38 +0200

    ULTIMO COMMIT DIA 18

---
+++ JS/operadores-javascript.js
@@ -0,0 +1,17 @@
+// suma
+const a = 2 + 3;
+document.write(a)
+// resta
+const b = 2 - 3;
+// multiplicación
+const c = 2 * 3;
+// división
+const d = 2 / 3;
+// módulo
+const e = 2 % 3;
+// incremento posterior
+a++;
+// incremento anterior
+++a; //Eqivalente a = a + 1
+document.write (a++)
+document.write ("a")
