1dc14043358869b43f73768ff8f6fb0af339af40
Cata2507 <105198441+Cata2507@users.noreply.github.com>
2022-05-19 13:54:27 +0200

    ULTIMO COMMIT DIA 19

---
+++ JS/bucle-for.js
@@ -0,0 +1,24 @@
+for (let i = 1;  i < 51; i++ ) {
+    // De uno al 50
+    console.log(i);
+    }
+
+const array = ['En', 'un', 'lugar', 'de', 'la', 'mancha'];
+const len = array.length; 
+// recorre un array mediante un for clásico
+for (let i = 0; i < len; i++) {
+// En, un, lugar, de, la, mancha (en distintas líneas)
+    console.log(array[i]);
+    document.write(array[i] + "")
+}
+
+// ejercicio 9 tablas de multiplicar
+for (let i = 0; i < 11; i++) {
+    //Tabla del 5X1 a 5x10)
+    document.write
+        console.log(array[i]);
+        document.write("5x"+ i + "=" + 5*1 +  "<br>")
+    }
+
+    // Ejercicio 10
+var palabra = prompt("Escribe cualquier palabra")
---
+++ JS/swith.js
@@ -0,0 +1,18 @@
+const a = 4;
+switch (a) {
+    case 1:
+    console.log('El valor de a es 1');
+    break;
+    case 2:
+    console.log('El valor de a es 2');
+    break;
+    case 34:
+    console.log('El valor de a es 3');
+    break;
+    case 4:
+    console.log('El valor de a es 4');
+    break;
+    default:
+    
+console.log('El valor de a es desconocido');
+}
